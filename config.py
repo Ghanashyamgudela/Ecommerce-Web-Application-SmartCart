d91440b48df5e908c73b016e34b382bf88c4be2e
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-05-07 21:14:04 +0530

    Refactor config.py to use environment variables
    Updated sensitive configuration values to use environment variables for security.

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ import os
 # =========================================================
 # APP SECRET
 # =========================================================
-SECRET_KEY = "abe123"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # =========================================================
 # RAZORPAY
 # =========================================================
-RAZORPAY_KEY_ID = "rzp_test_SlGbnJzNHYY2kL"
-RAZORPAY_KEY_SECRET = "6v9njW4QuWqc95585eUo7xwr"
+RAZORPAY_KEY_ID = os.environ.get("rzp_test_SlGbnJzNHYY2kL")
+RAZORPAY_KEY_SECRET = os.environ.get("6v9njW4QuWqc95585eUo7xwr")
 
 # =========================================================
 # POSTGRESQL / NEON DATABASE
 # =========================================================
-DATABASE_URL = 'postgresql://neondb_owner:npg_hT2HYGF1Qcnq@ep-aged-star-aqhctu3m.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require'
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # =========================================================
 # MAIL CONFIG
 # =========================================================
-MAIL_SERVER = 'smtp.gmail.com'
+MAIL_SERVER = 'smtp-relay.brevo.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'ghana19183@gmail.com'
-MAIL_PASSWORD = 'jmwe kafu lmdp cmpr'
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD =  os.environ.get("MAIL_PASSWORD")
 
 # =========================================================
 # SUPER ADMIN
