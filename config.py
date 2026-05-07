32ca579b6f342e553a2cf2db7feb772ed900caf2
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-05-07 22:51:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # =========================================================
 # RAZORPAY
 # =========================================================
-RAZORPAY_KEY_ID = os.environ.get("rzp_test_SlGbnJzNHYY2kL")
-RAZORPAY_KEY_SECRET = os.environ.get("6v9njW4QuWqc95585eUo7xwr")
+RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET")
 
 # =========================================================
 # POSTGRESQL / NEON DATABASE
@@ -19,7 +19,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 # =========================================================
 # MAIL CONFIG
 # =========================================================
-MAIL_SERVER = 'smtp-relay.brevo.com'
+MAIL_SERVER = os.environ.get("MAIL_SERVER")
+
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
