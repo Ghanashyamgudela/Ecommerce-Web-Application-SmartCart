fdf1cf4ae78ac97aa8440963ddc5393bbabdc18f
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-05-07 19:45:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ import os
 # =========================================================
 # APP SECRET
 # =========================================================
-SECRET_KEY = os.getenv("SECRET_KEY", "abe123")
+SECRET_KEY = "abe123"
 
 # =========================================================
 # RAZORPAY
 # =========================================================
-RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
-RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
+RAZORPAY_KEY_ID = "rzp_test_SlGbnJzNHYY2kL"
+RAZORPAY_KEY_SECRET = "6v9njW4QuWqc95585eUo7xwr"
 
 # =========================================================
 # POSTGRESQL / NEON DATABASE
 # =========================================================
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://neondb_owner:npg_hT2HYGF1Qcnq@ep-aged-star-aqhctu3m.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require'
 
 # =========================================================
 # MAIL CONFIG
 # =========================================================
-MAIL_SERVER = 'smtp-relay.brevo.com'
+MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_USERNAME = 'ghana19183@gmail.com'
+MAIL_PASSWORD = 'jmwe kafu lmdp cmpr'
 
 # =========================================================
 # SUPER ADMIN
