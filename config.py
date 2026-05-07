5cae77830a6e47c34e95052f192eb980c8a0cf4b
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-05-07 23:10:52 +0530

    Add MAIL_USE_SSL configuration to config.py

--- config.py
+++ config.py
@@ -22,6 +22,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 MAIL_SERVER = os.environ.get("MAIL_SERVER")
 
 MAIL_PORT = 587
+MAIL_USE_SSL = False
+
 MAIL_USE_TLS = True
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD =  os.environ.get("MAIL_PASSWORD")
